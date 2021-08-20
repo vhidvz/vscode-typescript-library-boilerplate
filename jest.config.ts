8f9ed788f10039e531d1c4c5f98d9634e4209ad4
Vahid V <vhid.vz@gmail.com>
2021-08-20 16:56:05 +0430

    updated

--- jest.config.ts
+++ jest.config.ts
@@ -1,42 +1,11 @@
 export default {
-  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
-  // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: ['/node_modules/'],
-
-  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
-
-  // A path to a custom dependency extractor
-  // dependencyExtractor: undefined,
-
-  // An array of directory names to be searched recursively up from the requiring module's location
-  // moduleDirectories: [
-  //   "node_modules"
-  // ],
-
-  // A preset that is used as a base for Jest's configuration
   preset: 'ts-jest',
-
-  // The root directory that Jest should scan for tests and modules within
-  // rootDir: undefined,
-
-  // A list of paths to directories that Jest should use to search for files in
   roots: ['<rootDir>/test'],
-
-  // The test environment that will be used for testing
   testEnvironment: 'node',
-
-  // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
-
-  // Whether to use watchman for file crawling
+  coverageReporters: ['json-summary', 'text', 'lcov'],
   watchman: true,
 };
