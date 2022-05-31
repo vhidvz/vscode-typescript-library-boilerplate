8a6331265b72d7c5df1d55fec8ab91353f119505
Vahid V <vhid.vz@gmail.com>
2022-05-31 15:34:57 +0430

    update

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,12 @@
 export default {
   collectCoverage: true,
-  coverageDirectory: 'coverage',
+  coverageDirectory: 'docs/coverage',
   coveragePathIgnorePatterns: ['/node_modules/'],
   coverageProvider: 'v8',
   preset: 'ts-jest',
   roots: ['<rootDir>/test'],
   testEnvironment: 'node',
+  setupFilesAfterEnv: ['./jest.setup.js'],
   coverageReporters: ['json-summary', 'text', 'lcov'],
   watchman: true,
 };
